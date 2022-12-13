33d4b94cf6a4cae3873e581fb3565bbfbbbdd0f0
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-12-13 16:53:55 -0500

    +input listener, +exit sequence, +setupInput call

--- play.js
+++ play.js
@@ -3,10 +3,22 @@ const { connect } = require("./client");
 console.log("Connecting ...");
 connect();
 
-const setupInput = function () {
+const setupInput = function() {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
+  // Input listener
+  stdin.on("data", handleUserInput);
   return stdin;
 };
+
+const handleUserInput = function(key) {
+  // Exit sequence handler (Ctrl+c)
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput();
+
