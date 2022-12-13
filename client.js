9b32ce3f27102a2a1d27c91cc2656d4844f4f896
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-12-13 16:31:56 -0500

    Sent name data to server on connection

--- client.js
+++ client.js
@@ -10,6 +10,8 @@ const connect = function () {
   
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
+
+    conn.write("Name: MCL");
   });
 
   conn.on("data", (data) => {
