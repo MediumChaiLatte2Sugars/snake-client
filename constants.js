a13c7f856af2e3fb90cfd7f497d0372b7403af68
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-12-13 20:50:03 -0500

    Added new constants for movesets and messages, exported

---
+++ constants.js
@@ -0,0 +1,19 @@
+const IP = "localhost";
+const PORT = 50541;
+
+const MOVE_SET = { 
+  w: "Move: up",
+  a: "Move: left",
+  s: "Move: down",
+  d: "Move: right",
+}
+
+const MESSAGES = {
+  l: "Say: LOL",
+  o: "Say: Wow!",
+  y: "Say: Oof!",
+  g: "Say: Hey!",
+  G: "Say: Good game!",
+}
+
+module.exports = { IP, PORT, MOVE_SET, MESSAGES };
