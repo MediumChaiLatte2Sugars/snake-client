3407d789c1f57d2c297207cb069e0e44caa59f06
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-12-13 20:19:39 -0500

    Send moves to server based on input using conn obj

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -9,9 +12,22 @@ const setupInput = function() {
 };
 
 const handleUserInput = function(key) {
-  // Exit sequence handler (Ctrl+c)
-  if (key === '\u0003') {
-    process.exit();
+ 
+  switch (key){
+    case 'w':
+      connection.write("Move: up");
+      break;
+    case 'a':
+      connection.write("Move: left");
+      break;
+    case 's':
+      connection.write("Move: down");
+      break;
+    case 'd':
+      connection.write("Move: right");
+      break;
+    case '\u0003': // Exit sequence (Ctrl+c)
+      process.exit();
   }
 };
 
