aef6cff50d1fd05c3d2455874dda2713621e40e1
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-12-13 21:05:50 -0500

    Linted, refactored to agree w/ linter

--- input.js
+++ input.js
@@ -14,20 +14,20 @@ const setupInput = function(conn) {
 
 const handleUserInput = function(key) {
   // Exit sequence handler (Ctrl+c)
-  if (key === '\u0003'){
+  if (key === '\u0003') {
     process.exit();
   }
 
   // Looks up appropriate key-binding for move, sends to server
-  if (MOVE_SET.hasOwnProperty(key)){
+  if (Object.keys(MOVE_SET).includes(key)) {
     connection.write(MOVE_SET[key]);
   }
 
   // Looks up appropriate key-binding for message, sends to server
-  if (MESSAGES.hasOwnProperty(key)){
+  if (Object.keys(MESSAGES).includes(key)) {
     connection.write(MESSAGES[key]);
   }
-  
+
 };
 
-module.exports = { setupInput }
+module.exports = { setupInput };
