2cdeb1495fcdc3b9123a4378595ae80567d772f2
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-12-13 21:06:09 -0500

    Linted

--- constants.js
+++ constants.js
@@ -1,12 +1,12 @@
 const IP = "localhost";
 const PORT = 50541;
 
-const MOVE_SET = { 
+const MOVE_SET = {
   w: "Move: up",
   a: "Move: left",
   s: "Move: down",
   d: "Move: right",
-}
+};
 
 const MESSAGES = {
   l: "Say: LOL",
@@ -14,6 +14,6 @@ const MESSAGES = {
   y: "Say: Oof!",
   g: "Say: Hey!",
   G: "Say: Good game!",
-}
+};
 
 module.exports = { IP, PORT, MOVE_SET, MESSAGES };
